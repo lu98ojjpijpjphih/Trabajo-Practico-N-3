be1114501945d42be79455bc1881a070fe1ec8d4
LuisanaGarello <164653068+lu98ojjpijpjphih@users.noreply.github.com>
2024-06-27 15:42:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//Actividad #1: Mostrá en la consola los números del 1 al 10
+
+for (let indice = 10; indice >0; indice=indice - 1){
+    console.log (indice)
+}
+   
+
+//Actividad #2: Mostrá en la consola los números del 15 al 5
+
+for (indice = 15; indice >4; indice=indice -1){
+    console.log (indice)
+}
+//Actividad #3: Mostrá en la consola los números impares del 1 al 100
+
+for (indice = 1; indice <= 100; indice=indice +2 ){
+    console.log (indice)
+}
+
+
+//Actividad #4: Mostrá en la consola la tabla de multiplicación del 7
+
+for (indice = 0; indice <= 70; indice=indice +7){
+    console.log (indice)
+}
+
+
+
+
+
